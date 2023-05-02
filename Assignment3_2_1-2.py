486ef0d361c0a89f909e47f326c0da0a11bba126
Martin Iversen <martiiv@stud.ntnu.no>
2023-05-02 16:02:16 +0200

    Working on cross validataion

--- Assignment3_2_1-2.py
+++ Assignment3_2_1-2.py
@@ -76,13 +76,15 @@ def trainModel(input, targetValues, learningRate, numOfEpoch):
         mse.append(meanSquaredError(target, outputNeuron.inputValue, numOfEpoch)) 
            
 
-    plt.plot(range(0,numOfEpoch), mse)
-    plt.xlabel("Epoch")
-    plt.ylabel("Mean square error")
-    plt.show()
+    #plt.plot(range(0,numOfEpoch), mse)
+    #plt.xlabel("Epoch")
+    #plt.ylabel("Mean square error")
+    #plt.show()
     return outputNeuron, neuron1, neuron2, neuron3
 
 def predictWithNN(x, y):
+    results = []
+    
     for i in range(0, len(x)):
         neuron1.inputValue = activationFunction(x.iloc[i], neuron1.weights)
         neuron2.inputValue = activationFunction(x.iloc[i], neuron2.weights)
@@ -90,12 +92,20 @@ def predictWithNN(x, y):
         outputNeuron.inputValue = activationFunction([neuron1.inputValue, neuron2.inputValue, neuron3.inputValue], outputNeuron.weights)
         
         if outputNeuron.inputValue >= 0.5:
-            print("Guess: Iris-versicolor actual value: ", y.iloc[i])
-            return 1
+            outputNeuron.inputValue = 1
+            if outputNeuron.inputValue == y.iloc[i]:
+                results.append(1)
+            
         elif outputNeuron.inputValue < 0.5:
-            print("Guess: Iris-setosa actual value: ", y.iloc[i])
-            return 0
+            outputNeuron.inputValue = 0
+            if outputNeuron.inputValue == y.iloc[i]:
+                results.append(1)
         
+    total = sum(results)
+    print("Total correct predictions: ", total, "\n")
+    print("Total predictions: ", len(x), "\n")
+    print("Accuracy: ", total/len(x), "\n")
+    
 # Function takes in input and weights and returns the output of one neuron by multiplying the input with asscoiated weights and adding the bias 
 # The output is then passed through the sigmoid function
 def activationFunction(input, weights): 
@@ -136,20 +146,19 @@ dataset = df.drop(["Iris Class"], axis=1)                                   # Dr
 # Task 2.2 is to perform 5-fold cross validation on the dataset 
 # I have used an online tutorial to implement the sklearn cross validation function: https://towardsdatascience.com/train-test-split-and-cross-validation-in-python-80b61beca4b6 
 # Last visit 02.05.2023
-kfold = KFold(n_splits=10) # Creating the kfold object
-kf = kfold.split(dataset, targetValues) # Splitting the dataset into 5 folds
+kfold = KFold(n_splits=5) # Creating the kfold object
+kf = kfold.split(dataset) # Splitting the dataset into 5 folds
     
 learningRate = 0.01
 numOfEpoch = 100
 
-output = trainModel(dataset, targetValues, learningRate, numOfEpoch)
-test = predictWithNN(dataset, targetValues)
 
-#for i, (train_index, test_index) in enumerate(kf):
-    #print(f"Fold {i}:")
-    #print(f"  Train: index={train_index}")
-    #print(f"  Test:  index={test_index}")
+#output = trainModel(dataset, targetValues, learningRate, numOfEpoch)
+#test = predictWithNN(dataset, targetValues)
 
+for i, (train_index, test_index) in enumerate(kf):
+    print(f"Fold {i+1}:")    
+    output = trainModel(dataset.iloc[train_index], targetValues.iloc[train_index], learningRate, numOfEpoch)
+    
+    test = predictWithNN(dataset.iloc[test_index], targetValues.iloc[test_index])
 
-
-#! Something wrong with output neurons weights probably line 74
